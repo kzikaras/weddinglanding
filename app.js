38c7f4d207f44f11dc64d1ca4f1aebfab21f313e
Kurt Zikaras <kurtzikaras@gmail.com>
2018-09-13 17:50:53 -0400

    adjusted photos for mobile

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var num = 0;
 setInterval(function(){
+    if(window.innerWidth <= 724){
+        console.log('mobile');
+        if(num === 0 || num === 2 || num === 11){
+            num++;
+        }
+    }
     $('.wrapper').css("background-image", "url(../images/"+ num +".jpg)");
     num++;
     if(num === 13){
         num = 0;
     }
-}, 2500)
+}, 3000)
